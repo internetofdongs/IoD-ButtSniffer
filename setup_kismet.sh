e5c68ff579f5c0d49b6d9e3b34c527cbbd3672df
RenderMan <info@internetofdon.gs>
2017-12-07 09:40:15 -0700

    Fixed mobile dashboard install

--- setup_kismet.sh
+++ setup_kismet.sh
@@ -7,23 +7,23 @@ sudo apt-get update && sudo apt-get -y dist-upgrade && sudo apt-get autoclean &&
 sudo apt-get -y install build-essential libmicrohttpd-dev git libnl-3-dev libnl-genl-3-dev libcap-dev libpcap-dev libncurses5-dev libnm-dev libdw-dev libsqlite3-dev libncurses5-dev libnm-dev libdw-dev libsqlite3-dev libbluetooth-dev
 
 #Clone the Kismet Git repo, configure and build, installing as suid root
-git clone https://github.com/kismetwireless/kismet.git
+git clone https://github.com/kismetwireless/kismet.git ~/kismet
 
 echo "If you are building this on a Raspberry Pi, go get a coffee, it'll take a while"
 
-cd kismet && ./configure && make && sudo make suidinstall
+cd ~/kismet && ./configure && make && sudo make suidinstall
 
 #Add current user to kismet group to run as suidroot
 sudo usermod -a -G kismet $USER
 
-#Copy Kismet configs
-cd ..
-cp kismet_httpd.conf kismet_site.conf kismet_logging.conf kismet_storage.conf ~/.kismet
-
-
 #Create ButtSniffer data directory
 cd ~/
 mkdir ~/ButtSniff_Data
+
+#Copy Kismet configs
+cd ~/IoD_ButtSniffer
+cp kismet_httpd.conf kismet_site.conf kismet_logging.conf kismet_storage.conf ~/.kismet
+
 
 #Install IoD-Screwdriver plugin
 cd ~/
@@ -32,6 +32,6 @@ cd IoD-Screwdriver/plugin-iod-screwdriver
 sudo make install
 
 #Install Kismet Mobile Dashboard
-cd ~/kismet
-git clone https://github.com/elkentaro/KismetMobileDashboard.git
-cd ~/kismet/KismetMobileDashboard && sudo make install
+
+git clone https://github.com/elkentaro/KismetMobileDashboard.git ~/IoD-ButtSniffer/kismet/KismetMobileDashboard
+cd ~/IoD-ButtSniffer/kismet/KismetMobileDashboard && sudo make install
