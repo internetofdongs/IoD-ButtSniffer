0766553b2bfb1299b2e749e971f6b34d815c0186
RenderMan <info@internetofdon.gs>
2019-01-30 04:14:12 +0000

    Updated kismet script

--- setup_kismet.sh
+++ setup_kismet.sh
@@ -5,17 +5,22 @@ sudo apt-get update && sudo apt-get -y dist-upgrade && sudo apt-get autoclean &&
 
 #Get dependancies for Kismet
 #sudo apt-get -y install build-essential libmicrohttpd-dev git libnl-3-dev libnl-genl-3-dev libcap-dev libpcap-dev libncurses5-dev libnm-dev libdw-dev libsqlite3-dev libncurses5-dev libnm-dev libdw-dev libsqlite3-dev libbluetooth-dev
-sudo apt-get install build-essential git libmicrohttpd-dev pkg-config zlib1g-dev libnl-3-dev libnl-genl-3-dev libcap-dev libpcap-dev libncurses5-dev libnm-dev libdw-dev libsqlite3-dev libprotobuf-dev libprotobuf-c-dev protobuf-compiler protobuf-c-compiler python python-protobuf python-sqlite python-requests librtlsdr0
+#sudo apt-get install build-essential git libmicrohttpd-dev pkg-config zlib1g-dev libnl-3-dev libnl-genl-3-dev libcap-dev libpcap-dev libncurses5-dev libnm-dev libdw-dev libsqlite3-dev libprotobuf-dev libprotobuf-c-dev protobuf-compiler protobuf-c-compiler python python-protobuf python-sqlite python-requests librtlsdr0
 
 #Clone the Kismet Git repo, configure and build, installing as suid root
-git clone https://github.com/kismetwireless/kismet.git ~/kismet
+#git clone https://github.com/kismetwireless/kismet.git ~/kismet
 
-echo "If you are building this on a Raspberry Pi, go get a coffee, it'll take a while"
+#echo "If you are building this on a Raspberry Pi, go get a coffee, it'll take a while"
 
-cd ~/kismet && ./configure && make && sudo make suidinstall
+#cd ~/kismet && ./configure && make && sudo make suidinstall
 
 #Add current user to kismet group to run as suidroot
-sudo usermod -a -G kismet $USER
+#sudo usermod -a -G kismet $USER
+
+wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key | sudo apt-key add -
+echo 'deb https://www.kismetwireless.net/repos/apt/release/stretch stretch main' | sudo tee /etc/apt/sources.list.d/kismet.list
+sudo apt update
+sudo apt install kismet2018
 
 #Create ButtSniffer data directory
 cd ~/
